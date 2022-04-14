101ca98c96d481608cc089ac5421cabb8d39ec47
Giupi De Luca <giupi.deluca@hotmail.com>
2022-04-14 17:22:14 -0400

    proxy finished

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -17,7 +17,20 @@ function upgrade(address _newAddress) public {
 }
 
 fallback() payable external {
-    
+    address implementation = currentAddress;
+    require(implementation != address(0));
+    bytes memory data = msg.data;
+
+    assembly {
+        let result := delegatecall(gas(), implementation, add(data, 0x20), mload(data), 0, 0)
+        let size := returndatasize()
+        let ptr := mload(0x40)
+
+        returndatacopy(ptr, 0, size)
+        switch result
+        case 0 {revert(ptr, size)}
+        case 1 {return(ptr, size)}
+    }
 }
 
 }
