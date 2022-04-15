a1cc8db89d0c38009d4ed06eb0b93f8366c037d1
Giupi De Luca <giupi.deluca@hotmail.com>
2022-04-15 11:08:55 -0400

    update Age

---
+++ contracts/AgeUpdate.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^ 0.8.0;
+pragma abicoder v2;
+
+import "./Storage.sol";
+
+contract AgeUpdate is Storage {
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "you're not the contract owner");
+        _;
+    }
+
+    function getAge() public view returns (uint) {
+       return _uintStorage["age"];
+    }
+
+    function setAge(uint _age) public onlyOwner {
+        _uintStorage["age"] = _age;
+    }
+}
--- migrations/2_deploy_proxy.js
+++ migrations/2_deploy_proxy.js
@@ -1,7 +1,29 @@
 const Age = artifacts.require("Age");
 const Proxy = artifacts.require("Proxy");
+const AgeUpdate = artifacts.require("AgeUpdate");
 
-module.exports = async function(deployer, network, accounts) {
-    const age = await Age.deployed();
-    const proxy = await Proxy.deployed(age.address);
-}
+module.exports = async function (deployer, network, accounts) {
+  // deploy new instance of both contracts
+  const age = await Age.new();
+  const proxy = await Proxy.new(age.address);
+
+  // Age is now deployed at the existing proxy address and the result points to a new variable 
+  // this allows us to interact with proxy through the functional contract Age
+  const proxyToAge = await Age.at(proxy.address);
+
+  // set age
+  await proxyToAge.setAge(33);
+  // get age
+  let ageGet = await proxyToAge.getAge();
+  console.log("Age before update: " + ageGet.toNumber());
+
+  // deploy new version of Age contract with some updates
+  const ageUpdate = await AgeUpdate.new();
+
+  // change the _cuurentAddress in Proxy
+  await proxy.upgrade(ageUpdate.address);
+  
+  // both values for age shoould match
+  ageGet = await proxyToAge.getAge();
+  console.log("Age after update: " + ageGet.toNumber());
+};
